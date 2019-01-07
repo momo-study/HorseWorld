d8fbad5bd3c1aa2e519460c29dda372a1dd8bd4b
momo-study <46442105+momo-study@users.noreply.github.com>
2019-01-07 17:56:34 +0800

    Add files via upload

---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+      development: {
+      host: "localhost",     // Localhost (default: none)
+      port: 8545,  
+      gas: 10,         // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      }
+    }
+};
